424759f65b8f480074e8ad487a4b849814d39696
EsraaFarhat <esraafarhat97@gmail.com>
2022-03-07 01:26:07 +0200

    postgress permissions

--- start.sh
+++ start.sh
@@ -30,15 +30,15 @@ npm -v
 cd ..
 
 # INSTALL POSTGRES
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 sudo apt-get -y update
 
 # sudo apt-get install postgresql
 
-sudo apt-get install postgresql-12 -y
+sudo apt install postgresql-12 -y
 
 sudo sed -i 's/local   all             postgres                                peer/local   all             postgres                                md5/'  /etc/postgresql/12/main/pg_hba.conf
 sudo sed -i 's/host    all             all             127.0.0.1/32           md5/host    all             all            0.0.0.0/0              md5/'  /etc/postgresql/12/main/pg_hba.conf
